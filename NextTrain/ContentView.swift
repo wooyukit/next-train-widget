ade6e3ac51219ec9d014ffaa73c956ac0f02508d
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2021-12-19 23:58:24 +0800

    First Initial the ios and widget project with swift ui

---
+++ NextTrain/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NextTrain
+//
+//  Created by WOO Yu Kit Vincent on 19/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NextTrain/NextTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextTrainApp.swift
+//  NextTrain
+//
+//  Created by WOO Yu Kit Vincent on 19/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct NextTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

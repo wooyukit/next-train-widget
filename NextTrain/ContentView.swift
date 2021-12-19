8527abe444a11677efc899aa004a93ce3cb4bbcf
WOO Yu Kit Vincent <vincent.woo@scmp.com>
2021-12-20 00:34:55 +0800

    UPDATED: the basic layout of the home page

--- NextTrain/ContentView.swift
+++ NextTrain/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                Text("Hello, world!")
+            }
+            .navigationBarTitle("Next Train Widget")
+        }
     }
 }
 
